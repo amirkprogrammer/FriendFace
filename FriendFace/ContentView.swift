db673212b0188fdd0c8eee7bb81a1590031801ed
amirkprogrammer <amir.m.kabiri@gmail.com>
2024-04-05 13:42:21 -0700

    - retrieve JSON from network and implement SwiftData

--- FriendFace/ContentView.swift
+++ FriendFace/ContentView.swift
@@ -5,17 +5,58 @@
 //  Created by Amir Kabiri on 4/3/24.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: \User.name) private var users: [User]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(users) { user in
+                NavigationLink(value: user) {
+                    HStack {
+                        Circle()
+                            .fill(user.isActive ? .green : .red)
+                            .frame(width: 30)
+                        
+                        Text(user.name)
+                    }
+                }
+            }
+            .navigationTitle("Friendface")
+            .navigationDestination(for: User.self) { user in
+                UserView(user: user)
+            }
+            .task {
+                await fetchUsers()
+            }
         }
-        .padding()
+    }
+    
+    func fetchUsers() async {
+        // Don't re-fetch data if we already have it
+        guard users.isEmpty else { return }
+        
+        do {
+            let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json")!
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            let downloadedUsers = try decoder.decode([User].self, from: data)
+            let insertContext = ModelContext(modelContext.container)
+            
+            for user in downloadedUsers {
+                insertContext.insert(user)
+            }
+            
+            try insertContext.save()
+        } catch {
+            print("Download failed")
+        }
+        
     }
 }
 
